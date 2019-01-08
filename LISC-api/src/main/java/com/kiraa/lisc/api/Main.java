d69709703ccbd2e332e28745dea908117edf90b3
kiraacorsac <wire150@gmail.com>
2019-01-08 13:34:39 +0100

    init

---
+++ LISC-api/src/main/java/com/kiraa/lisc/api/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kiraa.lisc.api;
+
+/**
+ *
+ * @author kiraa
+ */
+import static spark.Spark.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+}
